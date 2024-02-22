d2aaf29a7967ef2cd386c75834a4b9f5a68248fd
Sreelakshmi T U <149130349+sreelakshmi2312@users.noreply.github.com>
2024-02-22 17:01:15 +0900

    Added:Hive Fields

--- lib/Models/listModel.dart
+++ lib/Models/listModel.dart
@@ -1,6 +1,14 @@
+import 'package:hive/hive.dart';
+part 'listModel.g.dart';
+
+@HiveType(typeId: 1)
 class listItems{
+  @HiveField(0)
+  int?id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final String age;
-  listItems({required this.name, required this.age});
+  listItems({required this.name, required this.age , this.id});
 }
 
